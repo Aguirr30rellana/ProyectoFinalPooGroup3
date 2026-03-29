dc2f2cace9d76e54af28b0c717ede22037b848d8
alana <alana@host.docker.internal>
2026-03-28 18:15:35 -0600

    carga Inicial

---
+++ src/main/java/com/mycompany/proyectofinalpoogroup3/ProyectoFinalPooGroup3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinalpoogroup3;
+
+/**
+ *
+ * @author alana
+ */
+public class ProyectoFinalPooGroup3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
